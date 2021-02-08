ff0c534fb2e295770cdf6fdd095c2de350610550
Rafael Anguiano <rj.anguiano.23@gmail.com>
2021-02-08 11:42:52 -0600

    Agregando SplashScreen y pasando variables de MainStack a App.js

--- App.js
+++ App.js
@@ -4,36 +4,186 @@
  * Esta pantalla envía a MainStack
  */
 import React from 'react';
-import { StyleSheet} from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
 import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
-
+import * as SplashScreen from 'expo-splash-screen';
+import * as Location from 'expo-location';
+import * as firebase from 'firebase'
+import 'firebase/firestore';
 //Views Importadas
 import MainStack from './src/Navigation/MainStack';
 
-export default class App extends React.Component {
+const firebaseConfig = {
+  apiKey: "AIzaSyBjlhxDov6xAApWujA1eBooKTum_Yuvkjs",
+  authDomain: "hk-directions.firebaseapp.com",
+  databaseURL: "https://hk-directions-default-rtdb.firebaseio.com",
+  projectId: "hk-directions",
+  storageBucket: "hk-directions.appspot.com",
+  messagingSenderId: "115579744590",
+  appId: "1:115579744590:web:809f51b091efffae80ae1f"
+}
+firebase.initializeApp(firebaseConfig);
 
+const dbh = firebase.firestore();
+const consulta = dbh.collection('direccion').doc('LsMRnWB2pIUhxgMbeD54');
+
+export default class App extends React.Component {
+  
+  constructor(){
+    super()
+    this.state = {
+        //Variables para SecundSc y map
+        direccion: "",  //Variable Final
+        street: "",     //Calle de direccion
+        number: "",     //Número de dirección
+        district: "",   //Colónia de dirección
+        city: "",       //Ciudad de dirección
+        disable: false, //Estado de los botones
+        //Variables para Map
+        userLat: null, //
+        userLon: null,
+        //Variables para carga
+        appIsReady: false,
+        timer: false
+    }
+  }
+
+  //Primero en cargar
   async componentDidMount() {
+    //Componentes de carga de las pestañas
+    const doc = await consulta.get();
+    if (!doc.exists) { //Busca existencia de información en base de datos
+        console.log('No such document!');
+    } else {
+        console.log('Document data:', doc.data().place);
+    }
+    this.setState({direccion: doc.data().place })
+    const {coords} = await Location.getCurrentPositionAsync();  //Consiguiendo ubicación del usuario
+    this.setState({ //Definiendo las coordenadas iniciales
+        userLat: coords.latitude, 
+        userLon: coords.longitude
+    })
+    //Componentes de carga de la pantalla de carga
+    try {
+      await SplashScreen.preventAutoHideAsync();
+    } catch (e) {
+      console.warn(e);
+    }
+    this.prepareResources();
+    //Fonts para botones
     await Font.loadAsync({
        Roboto: require('native-base/Fonts/Roboto.ttf'),
        Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
        ...Ionicons.font,
     })
-}
+  }
 
+  //En caso de actualización de estado
+  async componentDidUpdate(prevState){
+    if(prevState.direccion !== this.state.direccion){
+        console.log("Enviar a DB")
+        if(this.state.direccion !== ""){
+            await  consulta.set({
+                place: this.state.direccion,
+            })
+        }
+        
+    }
+    console.log("Actualicé la db")
+  }
+
+  //Guarda la calle ingresada en la segunda pantalla
+  addStreet = (value)=>{
+    this.setState({street: value})
+  }
+  //Guarda el número ingresado en la segunda pantalla
+  addNumber = (value)=>{
+      this.setState({number: value})
+  }
+  //Guarda la colonia ingresada en la segunda pantalla
+  addDistrict = (value)=>{
+      this.setState({district: value})
+  }
+  //Guarda la ciudad ingresada en la segunda pantalla
+  addCity = (value)=>{
+      this.setState({city: value})
+  }
+
+  //Agrega la información de "texto" a "direccion", limpia "texto" y deshabilita botones
+  addToDirections = () => { 
+    this.setState({
+        direccion: this.state.street+" "+this.state.number +", "+this.state.district+", "+this.state.city,
+        //disable: true, //Deshabilita botones
+    })
+  }
+
+  //Consiguiendo los datos de ubicación desde lat y lon. Provenientes de MapSc
+  address = async (lat, lon) =>{
+    let addss = await Location.reverseGeocodeAsync({
+        latitude: lat, 
+        longitude: lon
+    })
+    console.log(addss[0])
+    this.setState({     //Guarda la información del lugar en "direccion"
+        direccion: addss[0].street+" "+addss[0].name+", "+addss[0].district+", "+addss[0].city,
+        street: addss[0].street,
+        number: addss[0].name,
+        district: addss[0].district,
+        city: addss[0].city,
+        //disable: true //Deshabilita botones
+    })
+  }
+
+  //Prepara todo antes de quitar la pantalla de carga
+  prepareResources = async () => {
+    try {
+      await performAPICalls();
+      await downloadAssets();
+    } catch (e) {
+      console.warn(e);
+    } finally {
+      this.setState({ appIsReady: true }, async () => {
+        await SplashScreen.hideAsync();
+      });
+    }
+  };
+
+  //RENDER
   render(){
+    if (!this.state.appIsReady) {
+      return (
+        <View style={{flex:1, backgroundColor:"gray", alignItems:'center', justifyContent:'center'}}>
+          <Text>Soy la pantalla de carga 👋</Text>
+        </View>
+      );
+    }
+
     return (
-      <MainStack/>
+      <MainStack
+        //Para Main
+        direccion={this.state.direccion} 
+        disable={this.state.disable}
+        //Para Secund
+        street={this.state.street}
+        number={this.state.number} 
+        district={this.state.district}  
+        city={this.state.city} 
+        addStreet={this.addStreet} 
+        addNumber={this.addNumber} 
+        addDistrict={this.addDistrict} 
+        addCity={this.addCity} 
+        addToDireccion={this.addToDirections}
+        //Para Map
+        userLat={this.state.userLat} 
+        userLon={this.state.userLon}
+        address={this.address}
+      />
     );
   }
   
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+async function performAPICalls() {}
+async function downloadAssets() {}
+
--- src/Navigation/MainStack.js
+++ src/Navigation/MainStack.js
@@ -5,116 +5,14 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import * as Location from 'expo-location';
-import * as firebase from 'firebase'
-import 'firebase/firestore';
 //Vistas
 import MainSc from '../screens/MainSc'
 import SecondSc from '../screens/SecondSc'
 import MapSc from '../screens/MapSc'
 
 const Stack = createStackNavigator();
-const firebaseConfig = {
-    apiKey: "AIzaSyBjlhxDov6xAApWujA1eBooKTum_Yuvkjs",
-    authDomain: "hk-directions.firebaseapp.com",
-    databaseURL: "https://hk-directions-default-rtdb.firebaseio.com",
-    projectId: "hk-directions",
-    storageBucket: "hk-directions.appspot.com",
-    messagingSenderId: "115579744590",
-    appId: "1:115579744590:web:809f51b091efffae80ae1f"
-}
-firebase.initializeApp(firebaseConfig);
-
-const dbh = firebase.firestore();
-const consulta = dbh.collection('direccion').doc('LsMRnWB2pIUhxgMbeD54');
 
 export default class MainStack extends React.Component {
-    constructor(){
-        super()
-        this.state = {
-            //Variables para SecundSc
-            direccion: "",  //Variable Final
-            street: "",     //Calle de direccion
-            number: "",     //Número de dirección
-            district: "",   //Colónia de dirección
-            city: "",       //Ciudad de dirección
-            disable: false, //Estado de los botones
-            //Variables para Map
-            userLat: null, //
-            userLon: null
-        }
-    }
-
-    async componentDidMount() {
-        const doc = await consulta.get();
-        if (!doc.exists) {
-            console.log('No such document!');
-        } else {
-            console.log('Document data:', doc.data().place);
-        }
-        this.setState({direccion: doc.data().place })
-        const {coords} = await Location.getCurrentPositionAsync();  //Consiguiendo ubicación del usuario
-        this.setState({ //Definiendo las coordenadas iniciales
-            userLat: coords.latitude, 
-            userLon: coords.longitude
-        })
-    }
-
-    async componentDidUpdate(prevState){
-        if(prevState.direccion !== this.state.direccion){
-            console.log("Enviar a DB")
-            if(this.state.direccion !== ""){
-                await  consulta.set({
-                    place: this.state.direccion,
-                })
-            }
-            
-        }
-        console.log("Actualicé la db")
-    }
-
-    //Guarda la calle ingresada en la segunda pantalla
-    addStreet = (value)=>{
-        this.setState({street: value})
-    }
-    //Guarda el número ingresado en la segunda pantalla
-    addNumber = (value)=>{
-        this.setState({number: value})
-    }
-    //Guarda la colonia ingresada en la segunda pantalla
-    addDistrict = (value)=>{
-        this.setState({district: value})
-    }
-    //Guarda la ciudad ingresada en la segunda pantalla
-    addCity = (value)=>{
-        this.setState({city: value})
-    }
-
-    //Agrega la información de "texto" a "direccion", limpia "texto" y deshabilita botones
-    addToDirections = () => { 
-        this.setState({
-            direccion: this.state.street+" "+this.state.number +", "+this.state.district+", "+this.state.city,
-            //disable: true, //Deshabilita botones
-        })
-    }
-    
-    //Consiguiendo los datos de ubicación desde lat y lon. Provenientes de MapSc
-    address = async (lat, lon) =>{
-        let addss = await Location.reverseGeocodeAsync({
-            latitude: lat, 
-            longitude: lon
-        })
-        console.log(addss[0])
-        this.setState({     //Guarda la información del lugar en "direccion"
-            direccion: addss[0].street+" "+addss[0].name+", "+addss[0].district+", "+addss[0].city,
-            street: addss[0].street,
-            number: addss[0].name,
-            district: addss[0].district,
-            city: addss[0].city,
-            //disable: true //Deshabilita botones
-        })
-    }
-
     render(){
         return (
             <NavigationContainer>
@@ -122,29 +20,29 @@ export default class MainStack extends React.Component {
                     <Stack.Screen name="Main" options={{title: 'Main Screen', headerShown:false}}>
                         {props => <MainSc 
                             {...props} 
-                            direccion={this.state.direccion} 
-                            disable={this.state.disable}/>}
+                            direccion={this.props.direccion} 
+                            disable={this.props.disable}/>}
                     </Stack.Screen>
                     <Stack.Screen name="Second" options={{title: 'Nueva Direccións', headerShown:true}}>
                         {props => <SecondSc 
                             {...props} 
-                            street={this.state.street}
-                            number={this.state.number} 
-                            district={this.state.district}  
-                            city={this.state.city} 
-                            addStreet={this.addStreet} 
-                            addNumber={this.addNumber} 
-                            addDistrict={this.addDistrict} 
-                            addCity={this.addCity} 
-                            addToDireccion={this.addToDirections} 
-                            disable={this.state.disable} />}
+                            street={this.props.street}
+                            number={this.props.number} 
+                            district={this.props.district}  
+                            city={this.props.city} 
+                            addStreet={this.props.addStreet} 
+                            addNumber={this.props.addNumber} 
+                            addDistrict={this.props.addDistrict} 
+                            addCity={this.props.addCity} 
+                            addToDireccion={this.props.addToDirections} 
+                            disable={this.props.disable} />}
                     </Stack.Screen>
                     <Stack.Screen name="Map" options={{title: 'Map Screen', headerShown:true}}>
                         {props => <MapSc 
                             {...props} 
-                            userLat={this.state.userLat} 
-                            userLon={this.state.userLon}
-                            address={this.address}
+                            userLat={this.props.userLat} 
+                            userLon={this.props.userLon}
+                            address={this.props.address}
                             />
                         }
                     </Stack.Screen>
